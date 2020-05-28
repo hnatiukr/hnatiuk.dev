afecc5d7e3126522453b9bf5219d53191b259c0c
Roman <gnatyuk.info@gmail.com>
2020-05-28 23:28:50 +0300

    change gitignore

---
+++ build/precache-manifest.a0638e79f5ccde08c00189a6498ccd0e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4305ebf1d69fe4f3ecde3a765e3c134f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9453061540b05a84f3e5",
+    "url": "/static/css/main.ac39df68.chunk.css"
+  },
+  {
+    "revision": "c9df177bf9c8979bd513",
+    "url": "/static/js/2.83da17f2.chunk.js"
+  },
+  {
+    "revision": "7cc9bc71dc193dc16a761d4db5c08eb5",
+    "url": "/static/js/2.83da17f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9453061540b05a84f3e5",
+    "url": "/static/js/main.914b096c.chunk.js"
+  },
+  {
+    "revision": "14e0cbd24e030684aaa1",
+    "url": "/static/js/runtime-main.40405824.js"
+  },
+  {
+    "revision": "6e92f6226ba6c0c2659a1ce9ab0b9233",
+    "url": "/static/media/Graphik-Medium.6e92f622.eot"
+  },
+  {
+    "revision": "988e0f8701a6a1a3f2a941d39c29e1d5",
+    "url": "/static/media/Graphik-Medium.988e0f87.ttf"
+  },
+  {
+    "revision": "e2fd4fd735768482753e7187a900a844",
+    "url": "/static/media/Graphik-Medium.e2fd4fd7.woff"
+  },
+  {
+    "revision": "2e79323ffcaba694589e254846f42fc5",
+    "url": "/static/media/Graphik-Regular.2e79323f.ttf"
+  },
+  {
+    "revision": "5398a3b447c9330f0ecd459086ee86a9",
+    "url": "/static/media/Graphik-Regular.5398a3b4.eot"
+  },
+  {
+    "revision": "fa20e17caff4a4cb0ac018fd08d2257e",
+    "url": "/static/media/Graphik-Regular.fa20e17c.woff"
+  },
+  {
+    "revision": "68309fdd26fc5a210e40f6fa0c638f4f",
+    "url": "/static/media/Graphik-Semibold.68309fdd.ttf"
+  },
+  {
+    "revision": "7530481a8f0cdfcf104ae93d3731b11e",
+    "url": "/static/media/Graphik-Semibold.7530481a.woff"
+  },
+  {
+    "revision": "bdecf3d042672dbb37807d1e1e2db376",
+    "url": "/static/media/Graphik-Semibold.bdecf3d0.eot"
+  },
+  {
+    "revision": "91c55ce7e822bd702d15037001a1fa65",
+    "url": "/static/media/sj-big.91c55ce7.jpg"
+  },
+  {
+    "revision": "59ad76b08711018617a51a137eab1994",
+    "url": "/static/media/sj-small.59ad76b0.jpg"
+  },
+  {
+    "revision": "0c4029320c921bb42c95212d1516e23e",
+    "url": "/static/media/up.0c402932.svg"
+  }
+]);
